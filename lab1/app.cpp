a2386eeb8a08f7ddb0fb6e05b5978dfe226ef8a8
Yahir215 <102612234+Yahir215@users.noreply.github.com>
2026-01-21 15:13:10 -0800

    Added a goodbye message

--- lab1/app.cpp
+++ lab1/app.cpp
@@ -3,5 +3,8 @@ using namespace std;
  
 int main(){ 
     cout << "WELCOME TO MY APP!" << endl; 
+    cout << "Goodbye World!" << endl;
+    cout << "This is a change" << endl;
+
     return 0;                                                                                       
 } 
