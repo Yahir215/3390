5f163aa175af0d712889e3690ba4b217280f0893
Yahir215 <102612234+Yahir215@users.noreply.github.com>
2026-01-21 17:32:20 -0800

    added addition and subtraction

--- lab1/app.cpp
+++ lab1/app.cpp
@@ -1,10 +1,23 @@
 #include <iostream> 
 using namespace std; 
- 
+
+int add(int x, int y){
+
+    return x + y; 
+}
+
+int subtract(int x, int y){
+
+    return x - y; 
+}
+
 int main(){ 
     cout << "WELCOME TO MY APP!" << endl; 
     cout << "Goodbye World!" << endl;
     cout << "This is a change" << endl;
+    cout << "Sum of 3 and 5 is: " << add(3, 5) << endl;
+    cout << "Difference of 10 and 4 is: " << subtract(10, 4) << endl;
 
-    return 0;                                                                                       
+    return 0;
+                                                                                       
 } 
